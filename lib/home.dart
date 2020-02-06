5715a61f2076caa8ec29925f04a9c7627fc8a946
tabe-dyna <tabe.unity@gmail.com>
2020-02-06 15:33:53 +0900

    before merging

--- lib/home.dart
+++ lib/home.dart
@@ -94,7 +94,9 @@ class Home extends StatelessWidget {
                 SizedBox(
                   height: 10.0,
                 ),
+                
                 StyledText(
+                  textAlign: TextAlign.center,
                   text: '<ios/> iOS / Flutter / Firebase / <unity/> Unity / Processing / <discord/> </twitter> Bot / <network/> IoT / <android/> Android / <react/> React / <html/> HTML & CSS',
                   styles: {
                     'ios': IconStyle(Fontisto.apple),
