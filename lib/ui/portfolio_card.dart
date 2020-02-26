ed98b6c72b8ae0e26e31b456bf7d6d7ecc7229cb
tabe <tabetabe.unity@gmail.com>
2020-02-26 12:18:55 +0900

    finish responsive layout to mobile and desktop

--- lib/ui/portfolio_card.dart
+++ lib/ui/portfolio_card.dart
@@ -136,107 +136,111 @@ class Details extends StatelessWidget {
         ),
       ),
       body: Padding(
-          padding: const EdgeInsets.all(10),
+          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
           child: SingleChildScrollView(
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    SizedBox(height: 25.0),
-                //popup left contents.
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.95,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      SizedBox(
-                        height: null,
-                        width: MediaQuery.of(context).size.width / 1.5,
-                        child: Image.asset(thumbnailImagePath),
-                      ),
-                      SizedBox(
-                        height: 30.0
-                      ),
-                      Text(
-                        description,
-                        textAlign: TextAlign.center,
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 30.0
-                ),
-                //popup right contents.
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.95,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        "Data of this app",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w700,
-                          fontSize: 20,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 20.0,
-                      ),
-                      Text("Title",
-                          style: TextStyle(fontWeight: FontWeight.w600)),
-                      SizedBox(height: 5),
-                      Text(name),
-                      SizedBox(
-                        height: 10.0,
-                      ),
-                      Text("Made with",
-                          style: TextStyle(fontWeight: FontWeight.w600)),
-                      SizedBox(height: 5),
-                      Text(usingTechnology),
-                      SizedBox(
-                        height: 10.0,
-                      ),
-                      Text("Supported Platform",
-                          style: TextStyle(fontWeight: FontWeight.w600)),
-                      SizedBox(height: 5),
-                      Text(supportedPlatform),
-                      SizedBox(
-                        height: 10.0,
-                      ),
-                      Text(
-                        "Programing Languaged Used",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontWeight: FontWeight.w600),
-                      ),
-                      SizedBox(
-                        height: 5.0,
-                      ),
-                      Text(programingLanguageUsed),
-                      SizedBox(height: 10.0),
-                      Text(
-                        "Donwloads",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontWeight: FontWeight.w600),
-                      ),
-                      SizedBox(
-                        height: 5.0,
-                      ),
-                      Text(downloads == 0
-                          ? "?"
-                          : "over " + downloads.toString()),
-                      SizedBox(height: 10.0),
-                      Text("Link",
-                          style: TextStyle(fontWeight: FontWeight.w600)),
-                      SizedBox(height: 5.0),
-                      Text("https://example.com"),
-                      SizedBox(height: 20.0,),
-                    ],
-                  ),
-                ),
-              ]))),
+              child: MediaQuery.of(context).size.width > 501 ? desktopDataWidget(context) : mobileDetailsWidget(context))),
     );
+  }
+
+  Widget mobileDetailsWidget(context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        SizedBox(height: 25.0),
+        //popup left contents.
+        Container(
+          width: MediaQuery.of(context).size.width * 0.95,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                height: null,
+                width: MediaQuery.of(context).size.width / 1.5,
+                child: Image.asset(thumbnailImagePath),
+              ),
+              SizedBox(
+                height: 30.0
+              ),
+              Text(
+                description,
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 30.0
+        ),
+        //popup right contents.
+        Container(
+          width: MediaQuery.of(context).size.width * 0.95,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Data of this app",
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 20,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text("Title",
+                  style: TextStyle(fontWeight: FontWeight.w600)),
+              SizedBox(height: 5),
+              Text(name),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text("Made with",
+                  style: TextStyle(fontWeight: FontWeight.w600)),
+              SizedBox(height: 5),
+              Text(usingTechnology),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text("Supported Platform",
+                  style: TextStyle(fontWeight: FontWeight.w600)),
+              SizedBox(height: 5),
+              Text(supportedPlatform),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                "Programing Languaged Used",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.w600),
+              ),
+              SizedBox(
+                height: 5.0,
+              ),
+              Text(programingLanguageUsed),
+              SizedBox(height: 10.0),
+              Text(
+                "Donwloads",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.w600),
+              ),
+              SizedBox(
+                height: 5.0,
+              ),
+              Text(downloads == 0
+                  ? "?"
+                  : "over " + downloads.toString()),
+              SizedBox(height: 10.0),
+              Text("Link",
+                  style: TextStyle(fontWeight: FontWeight.w600)),
+              SizedBox(height: 5.0),
+              Text("https://example.com"),
+              SizedBox(height: 20.0,),
+            ],
+          ),
+        ),
+    ]);
   }
 
   Widget desktopDataWidget(context) {
@@ -251,10 +255,13 @@ class Details extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                SizedBox(
-                  height: null,
-                  width: MediaQuery.of(context).size.width / 2.0,
-                  child: Image.asset(thumbnailImagePath),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 35.0),
+                  child: SizedBox(
+                    height: null,
+                    width: MediaQuery.of(context).size.width / 2.0,
+                    child: Image.asset(thumbnailImagePath),
+                  ),
                 ),
                 Text(
                   description,
@@ -265,7 +272,8 @@ class Details extends StatelessWidget {
           ),
           //popup right contents.
           Container(
-            width: MediaQuery.of(context).size.width / 10,
+            padding: EdgeInsets.symmetric(vertical: 35.0),
+            width: MediaQuery.of(context).size.width / 3.5,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
